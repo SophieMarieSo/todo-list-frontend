9b74e3255993296f3529021c10f44d874a92eaea
AhnEunJin <modic970515@gmail.com>
2024-05-25 01:00:44 +0900

    할 일 추가하기

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,32 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [todoValue, setTodoValue] = useState('');
 
   const getTasks = async () => {
     const resp = await api.get('/tasks');
     setTodoList(resp.data.data);
+  };
+
+  const addTask = async () => {
+    try {
+      const resp = await api.post('/tasks', {
+        task: todoValue,
+        isComplete: false,
+      });
+
+      if (resp.status === 200) {
+        // 문제
+        // 1. 입력창이 안 비워진다
+        setTodoValue('');
+        // 2. 추가한 할 일이 안보여진다
+        getTasks();
+      } else {
+        throw new Error('task can not be added');
+      }
+    } catch (error) {
+      console.log('ERROR', error);
+    }
   };
 
   useEffect(() => {
@@ -29,10 +51,14 @@ function App() {
             type='text'
             placeholder='할일을 입력하세요'
             className='input-box'
+            value={todoValue}
+            onChange={(e) => setTodoValue(e.target.value)}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button className='button-add'>추가</button>
+          <button className='button-add' onClick={addTask}>
+            추가
+          </button>
         </Col>
       </Row>
 
